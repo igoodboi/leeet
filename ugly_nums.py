4a1cd29aaacfaee5a29d41bcfc6f8825928e4e20
Samuel Wan <wanule123@gmail.com>
2024-05-13 18:13:48 -0700

    i made my code better so far no new leet code!!!!:):):) oh yeah use an assighnment operater

--- ugly_nums.py
+++ ugly_nums.py
@@ -27,17 +27,13 @@ class Solution:
     def isUgly(self, n: int) -> bool:
         if n <= 0:
             return False
-        # if n % 2 == 0:
-        #     print("false")
-        # else:
-        # print('true')
         while n > 1:
             if n % 2 == 0:
-                n = n // 2
+                n //= 2
             elif n % 3 == 0:
-                n = n // 3
+                n //= 3
             elif n % 5 == 0:
-                n = n // 5
+                n //= 5
             else:
                 return False
             # keep dividing
@@ -49,6 +45,6 @@ if __name__ == '__main__':
     # 6 = true
     # 1 = true
     # 14 = false
-    n = 1
+    n = 98
     answer = sol.isUgly(n)
     print(answer)
