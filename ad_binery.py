a482279f09520b73011c66d8fb2747348f8ee2bb
Samuel Wan <wanule123@gmail.com>
2024-08-11 15:18:33 -0700

    learned how to use bin

---
+++ ad_binery.py
@@ -0,0 +1,30 @@
+"""67. Add Binary
+Given two binary strings a and b, return their sum as a binary string.
+Example 1:
+Input: a = "11", b = "1"
+Output: "100"
+Example 2:
+Input: a = "1010", b = "1011"
+Output: "10101"
+Constraints:
+1 <= a.length, b.length <= 104
+a and b consist only of '0' or '1' characters.
+Each string does not contain leading zeros except for the zero itself.
+"""
+
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        num1 = int(a, 2)
+        num2 = int(b, 2)
+        sumthin = num1 + num2
+        binary_sum = bin(sumthin)
+        return binary_sum[2:]
+
+if __name__ == '__main__':
+    import sys
+
+    sys.setrecursionlimit(1501)
+    sol = Solution()
+    answer = sol.addBinary(a="1010", b="1011")
+    print(answer)
