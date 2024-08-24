72a6be0d5429d9498ffb30537c137a2f6d3ed227
Samuel Wan <wanule123@gmail.com>
2024-08-24 10:56:53 -0700

    i learned how to use the switch switchy a list into a str

--- ad_binery.py
+++ ad_binery.py
@@ -15,16 +15,37 @@ Each string does not contain leading zeros except for the zero itself.
 
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
+        if len(b) > len(a):
+            c = len(b) - len(a)
+            a = '0' * c + a
+        elif len(a) > len(b):
+            c = len(a) - len(b)
+            b = '0' * c + b
+        carry = 0
+        result = []
+        for i in range(len(a), 0,-1):
+            da = int(a[i-1])
+            db = int(b[i-1])
+            tot = da + db + carry
+            carry = tot // 2
+            result.append(str(tot % 2))
+        if carry == 1:
+            result.append(str(1))
+        return ''.join(result[::-1])
+
+    def addBinary1(self, a: str, b: str) -> str:
         num1 = int(a, 2)
         num2 = int(b, 2)
         sumthin = num1 + num2
         binary_sum = bin(sumthin)
         return binary_sum[2:]
 
+
 if __name__ == '__main__':
     import sys
 
     sys.setrecursionlimit(1501)
     sol = Solution()
-    answer = sol.addBinary(a="1010", b="1011")
-    print(answer)
+    answer1 = sol.addBinary(a="10101010101010101001", b="101010101110101")
+    answer2 = sol.addBinary1(a="10101010101010101001", b="101010101110101")
+    print(answer2,answer1)
